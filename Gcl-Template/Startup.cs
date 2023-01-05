fc646331d6e5fd4e095e7de787ab5889025dc378
TvantHooft <121797064+TvantHooft@users.noreply.github.com>
2023-01-05 08:38:29 +0100

    Initial commit

---
+++ Gcl-Template/Startup.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using GeeksCoreLibrary.Core.Extensions;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Gcl_Template
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment webHostEnvironment)
+        {
+            // First set the base settings for the application.
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{webHostEnvironment.EnvironmentName}.json", true, true);
+
+            // We need to build here already, so that we can read the base directory for secrets.
+            Configuration = builder.Build();
+
+            // Get the base directory for secrets and then load the secrets file from that directory.
+            var secretsBasePath = Configuration.GetSection("GCL").GetValue<string>("SecretsBaseDirectory");
+            if (!String.IsNullOrEmpty(secretsBasePath))
+            {
+                builder
+                    .AddJsonFile($"{secretsBasePath}appsettings-secrets.json", false, false)
+                    .AddJsonFile($"appsettings.{webHostEnvironment.EnvironmentName}.json", true, true);
+
+                // Build the final configuration with all combined settings.
+                Configuration = builder.Build();
+            }
+
+            // Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(Configuration)
+                .CreateLogger();
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to add services to the container.
+        /// </summary>
+        /// <param name="services"></param>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add Serilog.
+            services.AddLogging((builder) => { builder.AddSerilog(); });
+
+            // Add all GCL services.
+            services.AddGclServices(Configuration);
+
+            // Add MVC.
+            services.AddControllersWithViews();
+        }
+
+        /// <summary>
+        /// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        /// </summary>
+        /// <param name="app"></param>
+        /// <param name="env"></param>
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Add all GCL middle wares and other things the GCL needs, such as controller endpoints, routing, static files middle ware etc.
+            app.UseGclMiddleware(env);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Gcl-Template
+This project can be used as a template for creating a new project based on the Geeks Core Library.
+Click the button "Use this template" on the top-right in GitHub to create a new project based on this template.
+
+After creating the project, enter a connection string in the appsettings.json and then you're good to go!
